d021e8e11af3b1cb239e2bbdade3ad1ed913f6fe
HaritRane <90911979+HaritRane@users.noreply.github.com>
2021-12-23 13:21:02 +0530

    Update user page.py

--- user page.py
+++ user page.py
@@ -7,19 +7,23 @@ Created on Thu Dec 23 09:59:54 2021
 
 import tkinter as tk
 
-import main
+from main import New_Window
 
 
 root = tk.Tk()
 
+root.configure(bg='yellow')
+root.geometry("600x300")
+img = tk.PhotoImage(file=r'C:\Users\raneh\Downloads\backhang.png')
+label = tk.Label( root, image=img)
+label.place(x=0, y=0)
+L1=tk.Label(root,text="Enter Name",font=('Helvatica 10 bold ')).pack(anchor=tk.CENTER)
+E1=tk.Entry(root,bd=5,width=20)
+E1.pack(anchor=tk.CENTER)
 
-root.geometry("700x700")
 
-
-
-
-button = tk.Button(root, text="Click ME", bg='White', fg='Black',
-                              command=lambda:main.New_Window() )
+button = tk.Button(root, text="START", bg='White', fg='Black',
+                              command=lambda:New_Window() )
 
 button.pack(side='top')
 root.mainloop()
